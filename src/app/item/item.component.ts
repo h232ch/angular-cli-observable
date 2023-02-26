b4b88cdb60033bd14a64f5d0b7697b4b7d775e58
sean <sean.71@gmail.com>
2023-02-26 22:36:57 +0900

    Subject(Observable)'s item changed when 'toggle()' called by 'click' event in 'item.component.html' and 'console.log' indicates when the observable value changed

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,23 +1,29 @@
-import { Component, OnInit, OnDestroy, Input } from '@angular/core';
-import { Item } from '../item';
-import { ItemService } from '../item.service';
-import { Subscription } from "rxjs";
+import {Component, OnInit, OnDestroy, Input, OnChanges, SimpleChanges} from '@angular/core';
+import {Item} from '../item';
+import {ItemService} from '../item.service';
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.css']
 })
-export class ItemComponent implements OnInit, OnDestroy {
+export class ItemComponent implements OnInit, OnDestroy, OnChanges {
 
   @Input() item: Item;
   private itemSubscription: Subscription;
 
-  constructor(private itemService: ItemService) { }
+  constructor(private itemService: ItemService) {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+
+    }
 
   ngOnInit() {
     this.itemSubscription = this.itemService.$visible.subscribe((item: Item) => {
       this.item.isVisible = item.title === this.item.title;
+      console.log("item.title : " + item.title + " == " + "this.item.title : " + this.item.title)
     })
   }
 
